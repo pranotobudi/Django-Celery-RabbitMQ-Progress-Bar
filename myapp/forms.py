8ff0dca5ec79ecbcb15d84401ef6ff92b04bb99a
pranotobudi <pranotobudi.s@gmail.com>
2019-04-04 06:41:05 +0700

    create form and call celery task

---
+++ myapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.core.validators import MinValueValidator
+
+class GenerateRandomUserForm(forms.Form):
+    total_user = forms.IntegerField(
+        label='Number of users',
+        required=True,
+        validators=[
+            MinValueValidator(10)
+        ]
+    )
